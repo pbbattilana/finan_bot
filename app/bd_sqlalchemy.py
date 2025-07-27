86ff81a3760205bc2127d2535db2e4c45929aabc
pbbattilana <haddomears@hotmail.com>
2025-07-27 18:18:05 -0300

    delete duplications

--- app/bd_sqlalchemy.py
+++ app/bd_sqlalchemy.py
@@ -27,20 +27,6 @@ class TipoMovimiento(Base):
     id = Column(Integer, primary_key=True)
     nombre = Column(String, unique=True)
 
-
-
-class Entidad(Base):
-    __tablename__ = 'entidades'
-    id = Column(Integer, primary_key=True)
-    nombre = Column(String, unique=True)
-
-
-class TipoMovimiento(Base):
-    __tablename__ = 'tipos_movimiento'
-    id = Column(Integer, primary_key=True)
-    nombre = Column(String, unique=True)
-
-
 class Movimiento(Base):
     __tablename__ = 'movimientos'
 
